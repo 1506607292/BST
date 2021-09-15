cd028785caad2bdb2bdf5a21a4e95672a96e18dd
1506607292 <70865034+1506607292@users.noreply.github.com>
2021-09-15 08:47:49 +0800

    learning

--- DataStructure/CLearning/StackAndAddress.cpp
+++ DataStructure/CLearning/StackAndAddress.cpp
@@ -10,42 +10,54 @@ void or(int num,int p,...){
     printf("%X======\n",*((&num)));
     printf("%X======\n",*((&num)-1));
     char* temp = (char*)(&num);
-//    temp+=4;
-temp -= 1;
+    //    temp+=4;
+    temp -= 1;
     printf("%c\n",*temp);
     printf("%c\n",*(temp-1));
     printf("%c\n",*(temp-2));
     printf("%c\n",*(temp-3));
 }
+
+
+/*
+ * linux C的栈区 在我这个遍历器里面是由低到高的
+ * 而大部分人的编译器是由高到低的
+ * 在上边的不定参数测试中,只有向低地址寻址才能找到入栈的函数参数
+ *
+ * main函数里面解释了数组和指针以及数组指针的区别
+ *
+ * 数组可以直接+1  数组是连续 空间  但是数组指针实际上不可以 +1   这样会直接在二维数组中 出现错误 指向下一个一维数组
+ *
+ * 由于我是64位编译器  注意地址是64位  (本程序没有体现)
+ *
+ * */
 int main(){
     long long int ar[10];
 
     long  long int *pp[10];
     pp[0] = ar;
-    pp[1] = ar;
     printf("%p %p === \n",&pp,&pp[0]);
     for(long long int i=0;i<10;i++){
-//            pp[0][i] = 666;
+        //            pp[0][i] = 666;
         ((long long int*)*pp)[i] = 666;
     }
-
     *(((long long *)(*(pp+1)))+5) = 1433223;
     for(int i=0;i<10;i++){
-            printf("%lld\n",ar[i]);
+        printf("%lld\n",ar[i]);
     }
-//    or(args,0x32323232);
+    //    or(args,0x32323232);
 
     return 0;
-//    unsigned int lang = 50000000;
-//    unsigned long long *a = randUnsignedLong(lang);
-//    for(int ii=0;ii<lang;ii++){
-//        ArrayList array = ArrayList_New();
-//        if(array == NULL){
-//            return 0;
-//        }
-//        ArrayList_Destroy(array);
-//    }
-//    free(a);
+    //    unsigned int lang = 50000000;
+    //    unsigned long long *a = randUnsignedLong(lang);
+    //    for(int ii=0;ii<lang;ii++){
+    //        ArrayList array = ArrayList_New();
+    //        if(array == NULL){
+    //            return 0;
+    //        }
+    //        ArrayList_Destroy(array);
+    //    }
+    //    free(a);
 }
 //int main() {
 //    unsigned int lang = 5000;
